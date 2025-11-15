ff52cd07578c21d0f60fe695abe4524e021a4e1e
mohamed fathi <Mkhalifaali>
2025-11-15 18:49:25 +0200

    add SalaryComponentsController

---
+++ HRMS/DTOs/SalaryComponent/CreateSalaryComponentDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRMS.DTOs.SalaryComponent
+{
+    public class CreateSalaryComponentDto
+    {
+        [Required, MaxLength(150)]
+        public string ComponentName { get; set; } = string.Empty;
+
+        [Required]
+        public ComponentType ComponentType { get; set; }
+    }
+}
