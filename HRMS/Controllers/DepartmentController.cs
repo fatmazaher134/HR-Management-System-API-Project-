6c77b4562c62d8d807012d73c2131bc2b33fb1ef
Ibrahim Nazzier <ibrahemnazzier@yahoo.com>
2025-11-15 22:57:24 +0200

    Adding Admin APIs

--- HRMS/Controllers/DepartmentController.cs
+++ HRMS/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HRMS.Controllers
 {
@@ -27,18 +28,8 @@ namespace HRMS.Controllers
         public async Task<ActionResult<IEnumerable<DepartmentViewModel>>> GetDepartments()
         {
             var dtos = await _deptService.GetAllAsync();
-
-<<<<<<< HEAD
-            var viewModels = _mapper.Map<IEnumerable<DepartmentViewModel>>(dtos);
-
+            var viewModels = dtos.Select(d => _mapper.Map<DepartmentViewModel>(d)).ToList();
             return Ok(viewModels);
-=======
-            return employees.Select(e => new SelectListItem
-            {
-                Value = e.EmployeeID.ToString(),
-                Text = e.FullName
-            });
->>>>>>> EmployeeBranch
         }
 
         // GET: /api/department/5
